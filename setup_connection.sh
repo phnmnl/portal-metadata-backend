668cc7c743bc010e029fba174e2e88408885eeeb
Pablo Moreno <pcm32@users.noreply.github.com>
2017-07-20 13:29:53 +0100

    Removes echo of user

--- setup_connection.sh
+++ setup_connection.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $MYSQL_USER
 mv schema/propel.yaml schema/propel_orig.yaml
 sed "s/mysql:host=localhost;dbname=phenomenal/mysql:host=$MYSQL_HOST;dbname=phenomenal/" schema/propel_orig.yaml | \
 sed "s/user: root/user: $MYSQL_USER/" | \
