768528437a82d088a76f0dccaaf252477fe84b26
Pablo Moreno <pcm32@users.noreply.github.com>
2017-07-20 13:29:29 +0100

    Update setup_connection.sh

--- setup_connection.sh
+++ setup_connection.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+echo $MYSQL_USER
 mv schema/propel.yaml schema/propel_orig.yaml
-sed "s/dsn: \"mysql:host=localhost;dbname=phenomenal\"/dsn: \"mysql:host=$MYSQL_HOST;dbname=phenomenal\"/" schema/propel_orig.yaml | \
+sed "s/mysql:host=localhost;dbname=phenomenal/mysql:host=$MYSQL_HOST;dbname=phenomenal/" schema/propel_orig.yaml | \
 sed "s/user: root/user: $MYSQL_USER/" | \
 sed "s/password: 12345678/password: $MYSQL_PASSWORD/" > schema/propel.yaml
