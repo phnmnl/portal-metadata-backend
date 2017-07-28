1f4bd1644a827d939c18f30aaea03ca0a2a96ea7
Sijin He <sijin@ebi.ac.uk>
2017-07-28 15:01:49 +0100

    update jenkins url

--- src/controller/MetadataController.php
+++ src/controller/MetadataController.php
@@ -15,6 +15,18 @@ require __DIR__ . '/../service/MetadataService.php';
  * @apiSuccess {Boolean} Is Term and Condition agreed?
  */
 
+$app->get($apiPathPrefix.'/metadata/getJenkinsReport', function ($request, $response) {
+
+    $service = new MetadataService();
+
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->withJson($service->getJenkinsReport());
+
+});
+
 $app->get($apiPathPrefix.'/metadata/getGoogleKey', function ($request, $response) {
 
     $service = new MetadataService();
